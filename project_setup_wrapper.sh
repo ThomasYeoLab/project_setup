5ebe6f017bddc063fcadc201ecfd44bbae98fc7f
ahpxue <xueaihuiping@gmail.com>
2024-04-25 14:04:56 +0800

    Code for project setup
    Why:
    
    Add initial code
    
    This change addresses the need by:
    
    All

---
+++ project_setup_wrapper.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# This is a wrapper script to setup your personal project repo. 
+# You can download this single script and run. No need to clone the whole repo
+#
+# Input:
+#   -d: your project directory, including your repo name
+
+while getopts "d:" opt; do
+    case $opt in
+        d) repo_dir="$OPTARG"
+        ;;
+        \?) echo "Usage: cmd [-d] [-n](optional)"
+        ;;
+    esac
+done
+
+cd ${repo_dir}
+
+is_repo=$(git rev-parse --is-inside-work-tree 2>/dev/null)
+
+if [ "$is_repo" != "true" ]; then
+    echo "The folder ${repo_dir} is not a Git repository. "
+    repo_name=`basename ${repo_dir}`
+    git clone git@github.com:ThomasYeoLab/${repo_name}.git ${repo_dir}
+    exit_status=$?
+    if [ ! $exit_status -eq 0 ]; then
+        echo "Cannot find your repo ThomasYeoLab/${repo_name}, please contact github admin."
+        exit 1
+    fi
+fi
+
+echo "Creating directory... "
+if [ ! -d "${repo_dir}/setup/hooks" ]; then
+    mkdir -p ${repo_dir}/setup
+fi
+if [ ! -d "${repo_dir}/unit_tests" ]; then
+    mkdir -p ${repo_dir}/unit_tests
+fi
+if [ ! -d "${repo_dir}/examples" ]; then
+    mkdir -p ${repo_dir}/examples
+fi
+
+echo "Setup pre-push hook..."
+wget https://raw.githubusercontent.com/ThomasYeoLab/project_setup/main/hooks/pre-push -O ${repo_dir}/setup/hooks/pre-push
+chmod 755 ${repo_dir}/setup/hooks/pre-push
+ln -s ${repo_dir}/setup/hooks/pre-push ${repo_dir}/.git/hooks/pre-push
+
+echo "Update necessary files..."
+wget https://raw.githubusercontent.com/ThomasYeoLab/project_setup/main/update_setup.sh -O ${repo_dir}/setup/update_setup.sh
+sh ${repo_dir}/setup/update_setup.sh
+
+echo "ThomasYeoLab/${repo_name} setup finished!"
+
